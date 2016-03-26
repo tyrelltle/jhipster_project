169d122cbd8648965a13c4015ce3eff46c492bce
tyrelltle <tyrelltle@gmail.com>
2016-03-26 00:32:04 -0700

    user profile country update

--- src/main/webapp/scripts/components/entities/country/country.service.js
+++ src/main/webapp/scripts/components/entities/country/country.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('haklistApp')
-    .factory('Country', function ($resource, DateUtils) {
+    .factory('Country', function ($resource) {
         return $resource('api/country/:id', {}, {
             'query': {method: 'GET', isArray: true},
             'get': {

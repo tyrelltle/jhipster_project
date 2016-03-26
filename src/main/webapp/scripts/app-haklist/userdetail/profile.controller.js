a8851e6d0feddb10454e8e6da3751a11a5fb35ea
tyrelltle <tyrelltle@gmail.com>
2016-03-25 20:20:57 -0700

    distinguish update and create mode in user profile page

--- src/main/webapp/scripts/app-haklist/userdetail/profile.controller.js
+++ src/main/webapp/scripts/app-haklist/userdetail/profile.controller.js
@@ -4,7 +4,7 @@ angular.module('haklistUserApp')
     .controller('ProfileController', function ($scope,$state,Principal,UserProfile) {
 
         $scope.registerAccount={};
-
+        $scope.editmode=true;
          UserProfile.current({},function(result){
             $scope.registerAccount=result;
          });

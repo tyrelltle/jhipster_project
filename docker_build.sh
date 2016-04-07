07a6d225cb270562414203c3d2967752d2cf4fa0
tyrelltle <tyrelltle@gmail.com>
2016-04-06 23:40:14 -0700

    improving docker deployment script

--- docker_build.sh
+++ docker_build.sh
@@ -3,3 +3,8 @@
 cp src/main/docker/Dockerfile build/libs
 cd build/libs
 docker build -t haklist .
+#https://linuxconfig.org/remove-all-containners-based-on-docker-image-name
+docker ps -a | awk '{ print $1,$2 }' | grep tyrelltle/hacker | awk '{print $1 }' | xargs -I {} docker rm {}
+docker images | awk '{ print $1,$2 }' | grep tyrelltle/hacker | awk '{print $1 }' | xargs -I {} docker rmi {}
+docker images | awk '{ print $1,$2,$3 }' | grep '^haklist .*' | awk '{print $3 }' | xargs -I {} docker tag {} tyrelltle/hacker
+docker push tyrelltle/hacker
